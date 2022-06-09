4943378a679a83bb532b387f159065f25d575087
Jesse Ding <jding@Jesses-Air.fios-router.home>
2022-06-09 18:48:49 -0400

    first commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profile_image from "../assets/IMG_5804.JPG";  
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div class="split left">
+          <div className="centered">
+            <img
+              src = {profile_image} 
+              className="profile_image"
+              alt="Profile Pic"
+              ></img>
+          </div>
+        </div>
+        <div className="split right">
+          <div className="centered">
+            <div className="name_title">Jesse Ding</div>
+            <div className="brief_description">
+              I am a sophomore majoring in computer science at Carnegie Mellon.
+            </div>
+          </div>
+        </div> 
       </div>
     )
   }
